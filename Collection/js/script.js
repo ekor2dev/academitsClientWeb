5d9cbe891d599aa95658622a06b589e0dc2113a9
ek2dev_imac <turokg@yahoo.com>
2018-06-17 23:20:46 +0700

    firstCommit

---
+++ Collection/js/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    $("#buttonTask1").click(function () {
+        sortAndFilter()
+    });
+
+    $("#buttonTask2").click(function () {
+        getSquares();
+    });
+
+    function sortAndFilter() {
+        var size = 15;
+        var initial = Array(size).fill(5).map(function () {
+            return Number.parseInt((Math.random() * 100));
+        });
+
+        $("#initialTask1").text(initial);
+        $("#3sorted").text(initial.slice(0).sort().reverse());
+        $("#first5").text(initial.slice(0, 5));
+        $("#last5").text(initial.slice(0).reverse().slice(0, 5));
+        $("#evensSum").text(initial.filter(function (n) {
+            return n % 2 === 0;
+        }).reduce(function (a, b) {
+                return a + b;
+            }, 0
+        ))
+        ;
+    }
+
+    function getSquares() {
+        var size = 100;
+        var array = Array(size).fill(5).map(function (n, i) {
+                return i + 1;
+            }
+        );
+
+        $("#initialTask2").text(array);
+        $("#outputTask2").text(array.map(function (n) {
+                return n * n;
+            })
+        )
+        ;
+
+
+    }
+
+});
+
+
---
+++ SqareEquation/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $("input").change(function () {
+        calculate()
+    });
+
+    function calculate() {
+        var a = $("input#a").val();
+        var b = $("input#b").val();
+        var c = $("input#b").val();
+
+        var descr = b * b - 4 * a * c;
+        var x1 = (-b + Math.sqrt(descr)) / 2 / a;
+        var x2 = (-b - Math.sqrt(descr)) / 2 / a;
+        $("#x1").text(x1.toFixed(2));
+        $("#x2").text(x2.toFixed(2));
+    }
+
+});
+
+
